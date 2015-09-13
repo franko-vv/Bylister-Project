af12dd8f7cc274f97ff1ecee28d67ae275c66c35
f1tzgerald <alter.vetal@mail.ru>
2015-09-13 13:59:02 +0300

    Add class for player

---
+++ Assets/Scripts/CharacterCollider2D.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCollider2D : MonoBehaviour 
+{
+    // Кожа героя состоит из лучей, который воспринимают коллизии
+    private const float SkinWidth = .2f;
+    private const int TotalHorizontalRays = 8;
+    private const int TotalVerticalRays = 4;
+
+    private static readonly float SlopeLimitTangant = Mathf.Tan(75f * Mathf.Rad2Deg);
+
+    public LayerMask PlatformMask;
+    public ControllerParameters2D DefaultParameters;
+
+    public ControllerState2D State { get; private set; }
+
+    public bool CanJump { get; set; }
+    public Vector2 Velocity { get; private set; }
+
+    public void Awake()
+    {
+        State = new ControllerState2D();
+    }
+
+    public void AddForce(Vector2 force)
+    {
+    }
+
+    public void SetForce(Vector2 force)
+    {
+    }
+
+    public void SetHorizontalForce(float x)
+    {
+    }
+
+    public void SetVerticalForce(float y)
+    {
+    }
+
+    public void Jump()
+    {
+    }
+
+    public void LateUpdate()
+    {
+    }
+
+    public void Move(Vector2 deltaMovement)
+    {
+    }
+
+    private void HandlePlatforms()
+    {
+    }
+
+    private void CalculateRayOrigins()
+    {
+    }
+
+    private void MoveHorizontally(ref Vector2 deltaMovement)
+    {
+    }
+
+    private void MoveVertically(ref Vector2 deltaMovement)
+    {
+    }
+
+    private void HandleVerticalSlope(ref Vector2 deltaMovement)
+    {
+    }
+
+    private void HandleHorizontalSlope(ref Vector2 deltaMovement)
+    {
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+    }
+
+}
---
+++ Assets/Scripts/ControllerState2D.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerState2D
+{
+    public bool IsColidingRight { get; set; }
+    public bool IsColidingLeft { get; set; }
+    public bool IsColidingAbove { get; set; }
+    public bool IsColidingBelow { get; set; }
+    public bool IsMovingDownSlope { get; set; }
+    public bool IsMovingUpSlope { get; set; }
+    public bool IsGrounded { get { return IsColidingBelow; } }
+    public float SlopeAngle { get; set; }
+
+    public bool SetCollisions { get { return IsColidingRight || IsColidingLeft || IsColidingBelow || IsColidingAbove; } }
+
+    public void Reset()
+    {
+        IsColidingAbove = IsColidingBelow = IsColidingLeft = IsColidingRight = IsMovingDownSlope = IsMovingUpSlope = false;
+
+        SlopeAngle = 0;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("(controller): right:{0}  left:{1}  above:{2}  below: {3}  down-slope: {4}  up-slope: {5}  angle: {6}",
+            IsColidingRight, IsColidingLeft, IsColidingAbove, IsColidingBelow, IsMovingDownSlope, IsMovingUpSlope, SlopeAngle);
+    }
+
+}
